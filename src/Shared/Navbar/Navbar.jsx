f1bd6fa40a88d1546950c33ec5ca4b6595e4c1ee
Md. Abir Rahaman <abirahman.cse@gmail.com>
2022-06-07 01:04:20 +0600

    start the project

---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+            <div className=""></div>
+
+
+            
+        </div>
+    );
+};
+
+export default Navbar;
